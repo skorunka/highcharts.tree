28b67aa9a00c03e2f245b0cd33f9fa3fc2d7e4dc
Frantisek Skorunka <skorunka@outlook.com>
2016-10-30 15:34:31 +0000

    no message

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+﻿/// <binding AfterBuild='Tree:build' Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+	rimraf = require("rimraf"),
+	concat = require("gulp-concat"),
+	cssmin = require("gulp-cssmin"),
+	uglify = require("gulp-uglify"),
+	ts = require("gulp-typescript");
+
+var onError = function (err) {
+	console.log(err);
+};
+
+var config = {
+	sources: ["./src/**/*.js"],
+	outputDir: "./dist/",
+	outputFile: "tree.js"
+};
+
+gulp.task("build",
+	function () {
+		return gulp.src(config.sources)
+			.pipe(concat(config.outputFile))
+			.pipe(gulp.dest(config.outputDir));
+	});
